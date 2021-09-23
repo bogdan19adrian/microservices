00bdcd74d5cd3800710286e03b39ab5b451b04e0
bogdan19adrian <bogdan19adrian@gmail.com>
2021-09-23 19:53:28 +0300

    add eureka discovery

--- SR-gen1/src/main/java/ro/go/bogdanenache/client/SrGenClient.java
+++ SR-gen1/src/main/java/ro/go/bogdanenache/client/SrGenClient.java
@@ -1,10 +1,12 @@
 package ro.go.bogdanenache.client;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ro.go.bogdanenache.dto.MessageDTO;
 
@@ -14,15 +16,23 @@ import java.time.LocalDateTime;
 @Component
 public class SrGenClient {
 
-    @Value("${service.url}")
-    String url;
+    @Value("${sr2.service}")
+    String serviceName;
+
+    @Autowired
+    private EurekaClient eurekaClient;
 
     @Autowired
     private RestTemplate template;
 
     public MessageDTO doCall(MessageDTO dto) {
+        Application application = eurekaClient.getApplication(serviceName);
+        InstanceInfo instanceInfo = application.getInstances().get(0);
+        String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort();
+        System.out.println("URL" + url);
+
         log.info("Inside SrGen1Client");
         dto.getMessage().put(LocalDateTime.now().toString(), "SrGen1Controller");
-        return template.postForObject(url, dto, MessageDTO.class);
+        return template.postForObject(instanceInfo.getHomePageUrl(), dto, MessageDTO.class);
     }
 }
--- SR-gen2/src/main/java/ro/go/bogdanenache/client/SrGenClient.java
+++ SR-gen2/src/main/java/ro/go/bogdanenache/client/SrGenClient.java
@@ -1,5 +1,8 @@
 package ro.go.bogdanenache.client;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,13 +16,22 @@ import java.time.LocalDateTime;
 @Component
 public class SrGenClient {
 
-    @Value("${service.url}")
-    String url;
+    @Value("${sr3.service}")
+    String serviceName;
+
+    @Autowired
+    private EurekaClient eurekaClient;
 
     @Autowired
     private RestTemplate template;
 
     public MessageDTO doCall(MessageDTO dto) {
+        Application application = eurekaClient.getApplication(serviceName);
+        InstanceInfo instanceInfo = application.getInstances().get(0);
+        String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort();
+
+        System.out.println("URL" + url);
+
         log.info("Inside SrGen2Client");
         dto.getMessage().put(LocalDateTime.now().toString(), "SrGen2Controller");
         return template.postForObject(url, dto, MessageDTO.class);
